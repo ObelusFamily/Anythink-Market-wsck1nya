9614ce3360e7bb3f7d22c57d4530ae056e2cf98b
Rana Zarour <ranazya@gmail.com>
2023-02-15 14:53:00 +0000

    Fix the placeholder in the preview file

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -47,17 +47,12 @@ class Item extends React.Component {
         <div className="text-dark">
           <div className="row bg-white p-4">
             <div className="col-6">
-              {this.props.item.image ? <img
+              <img
                 src={this.props.item.image}
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
-              /> : <img
-              src={'../../placeholder.png'}
-              alt={this.props.item.title}
-              className="item-img"
-              style={{ height: "500px", width: "100%", borderRadius: "6px" }}
-            />}
+              />
             </div>
 
             <div className="col-6">
